795b49d12b4da2c3c2ed259f347a9e34a7295e6a
nurullahbozkurt <nurullahbozkurtt@gmail.com>
2021-10-16 14:53:45 +0300

    fix: cors issue

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json"); // <== Will be created later
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3200; // <== You can change the port
+
+server.use(
+  cors({
+    origin: true,
+    credentials: true,
+    preflightContinue: false,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  })
+);
+server.options("*", cors());
 
 server.use(middlewares);
 server.use(router);
